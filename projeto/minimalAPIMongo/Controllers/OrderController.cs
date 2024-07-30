28828011872d7a1084ee87c2ba85a3659eb93224
joao-henrike <133373332+joao-henrike@users.noreply.github.com>
2024-07-30 17:10:44 -0300

    Add files via upload

---
+++ projeto/minimalAPIMongo/Controllers/OrderController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using minimalAPIMongo.Domains;
+using minimalAPIMongo.Service;
+using minimalAPIMongo.ViewModels;
+using MongoDB.Driver;
+
+namespace minimalAPIMongo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMongoCollection<Order>? _order;
+        private readonly IMongoCollection<Client>? _client;
+        private readonly IMongoCollection<Product>? _product;
+
+        public OrderController(MongoDbService mongoDbService)
+        {
+            _order = mongoDbService.GetDatabase?.GetCollection<Order>("order");
+            _client = mongoDbService.GetDatabase?.GetCollection<Client>("client");
+            _product = mongoDbService.GetDatabase?.GetCollection<Product>("product");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Order>> Create(OrderViewModel orderViewModel)
+        {
+            try
+            {
+                Order order = new Order();
+
+                order.Id = orderViewModel.Id;
+                order.Date = orderViewModel.Date;
+                order.Status = orderViewModel.Status;
+                order.ProductId = orderViewModel.ProductId;
+                order.ClientId = orderViewModel.ClientId;
+
+                var client = await _client.Find(x => x.Id == order.ClientId).FirstOrDefaultAsync();
+
+                if (client == null)
+                {
+                    return NotFound("Cliente não existe");
+                }
+                order.Client = client;
+
+                await _order.InsertOneAsync(order);
+
+                return StatusCode(201, order);
+            }
+            catch (Exception e)
+            {
+                // Logar a exceção para diagnóstico
+                return StatusCode(500, $"Erro interno: {e.Message}");
+            }
+        }
+    }
+}
